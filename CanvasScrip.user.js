32ebba06e00eb78649876475c2e13e981b4672b4
RokuOEK <54308062+RokuOEK@users.noreply.github.com>
2022-04-04 20:46:09 +0100

    Update CanvasScrip.user.js

--- CanvasScrip.user.js
+++ CanvasScrip.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         RLJ Template
+// @name         RLJ Canvas Helper
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Roku LEMMiNO/JPN Canvas Helper
+// @version      0.2
+// @description  A canvas helping tool for LEMMiNO and J-Music fans
 // @author       Roku
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
@@ -13,8 +13,9 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/RokuOEK/RLJ-Canvas-Helper/main/jmusmap2.png";
-            i.style = "position: fixed;left: 0;top: 0;opacity: 0.8;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            i.src = "https://i.imgur.com/v5DfRVf.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
+            console.log(i);
             return i;
         })())
 
